2e7b00902acd88783c22f850d5c8ccb2b4b7846f
Shariarx6t9 <najmulhasanshahin@email.com>
2026-03-15 15:30:11 +0600

    remove next from async pre-save hook for mongoose v9

--- models/User.js
+++ models/User.js
@@ -18,15 +18,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  try {
-    if (!this.isModified("password")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
